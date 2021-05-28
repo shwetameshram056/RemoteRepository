83ef766c639db960154454580834f9d0dc3b1602
Shweta Meshram <shweta.meshram@ibm.com>
2021-05-28 14:56:48 +0530

    Created Def.java

--- a.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() 
-{
-   printf("Hello world in C!");
-   return 0;
-}
--- b.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-int main()
-{
-  cout << "Hello World in C++!";
-  return 0;
-}
